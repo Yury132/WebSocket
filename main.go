1c7639272bb482c55aa79201ce7c92c88ab37824
Yury132 <usynin.yury@yandex.ru>
2023-11-08 16:14:36 +0300

    tt
    tt

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -16,6 +17,14 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
+// Массив подключений
+type chatStruct struct {
+	ws []*websocket.Conn
+}
+
+// Глобальная переменная
+var chat = chatStruct{}
+
 // define a reader which will listen for
 // new messages being sent to our WebSocket
 // endpoint
@@ -24,22 +33,54 @@ func reader(conn *websocket.Conn) {
 		// read in a message
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			log.Println("Ошибка при чтении: ", err)
+			// Закрываем соединение
+			err = conn.Close()
+			if err != nil {
+				log.Println("Ошибка при закрытии соединения: ", err)
+				return
+			}
+			// Вычисляем индекс удаляемого подключения
+			index := indexOf(conn, chat.ws)
+			// Удаляем подключение из массива по индексу
+			chat.ws = append(chat.ws[:index], chat.ws[index+1:]...)
+			fmt.Printf("Количество подключений: %v\n", len(chat.ws))
 			return
 		}
 		// print out that message for clarity
-		log.Println(string(p))
+		log.Println("Пришло сообщение: ", string(p))
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
+		// Проходимся по всем подключениям и всем направляем сообщение
+		for i, c := range chat.ws {
+			if err := c.WriteMessage(messageType, p); err != nil {
+				log.Println(i, " : ", err)
+				return
+			}
 		}
 
 	}
 }
 
+// Вычисляем индекс элемента в срезе для Подключений
+func indexOf(conn *websocket.Conn, data []*websocket.Conn) int {
+	for k, v := range data {
+		if conn == v {
+			return k
+		}
+	}
+	return -1
+}
+
+// Стартовая страница
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
+
+	tmpl, err := template.ParseFiles("./index.html")
+	if err != nil {
+		fmt.Println("filed to show home page")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -51,10 +92,17 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Println("Client Connected")
+	//log.Println("WebSocket", ws)
+
+	// Добавляем каждое подключение в массив
+	chat.ws = append(chat.ws, ws)
+	fmt.Printf("Количество подключений: %v\n", len(chat.ws))
+
 	err = ws.WriteMessage(1, []byte("Hi Client!"))
 	if err != nil {
 		log.Println(err)
 	}
+
 	// listen indefinitely for new messages coming
 	// through on our WebSocket connection
 	reader(ws)
